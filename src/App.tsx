9b28dbef85475103e07dea1de7ac106cfd62c1eb
HireMeFullTime <pracownik2017@gmail.com>
2025-05-09 17:54:54 +0200

    Added basic configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import BasketPage from './pages/BasketPage';
+import HomePage from './pages/HomePage';
+import SummaryPage from './pages/SummaryPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className='App'>
+      <BrowserRouter>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/basket' element={<BasketPage />} />
+				<Route path='/summary' element={<SummaryPage />} />
+			</Routes>
+      </BrowserRouter>
+		</div>
+	);
 }
 
-export default App
+export default App;
