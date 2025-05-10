cd58536055688c14310c6dd37c8621ca7c4b1280
HireMeFullTime <pracownik2017@gmail.com>
2025-05-10 12:04:41 +0200

    Changed spelling on base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Katarzyna_Chmielecka_Web_Wroclaw"
+  base: "/Katarzyna_Chmielecka__Web_Wroclaw"
 })
